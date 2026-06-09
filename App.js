e18b479821f9ca1f098479f9986b9b0e3349f26d
Android Studio <devandroid@ort>
2026-06-09 09:21:30 -0300

    siguiendo el tp

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Image, SafeAreaView, Text, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+  const titleText = 'Login App (Laizerovich, Jajurin)';
+  const [nombre, handleNombreChange] = React.useState('');
+  const [contraseña, handlecontraseñaChange] = React.useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={[
+        styles.container,
+        { flexDirection: 'column', backgroundColor: '#f1f0f1' },
+      ]}
+    >
+      <StatusBar style="dark" />
+
+      <View style={{ flex: 2, backgroundColor: '#612ffc' }}>
+        <Text style={styles.titleText}>{titleText}</Text>
+      </View>
+
+      <View style={{ flex: 6 }}>
+        <Image
+          style={[styles.image, { resizeMode: 'center', marginTop: 30 }]}
+          source={require('./assets/Coca-Cola_logo.svg.png')}
+        />
+      </View>
+
+      <View style={{ flex: 4, backgroundColor: '#ffffff' }}>
+        <TextInput
+          style={styles.input}
+          placeholder="Ingrese su Email"
+          value={nombre}
+          onChangeText={handleNombreChange}
+        />
+          <TextInput
+          style={styles.input}
+          placeholder="Ingrese su contraseña"
+          value={contraseña}
+          onChangeText={handlecontraseñaChange}
+        />
+      </View>
+
+      <View style={{ flex: 4, backgroundColor: '#75ce39' }} >
+<Button title="Iniciar Sesión"
+
+onPress={() => Alert.alert('se ha presionado')}
+/>
+            </View>
+      <View style={{ flex: 5, backgroundColor: '#bbff00' }} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+  },
+  image: {
+    width: 400,
+    height: 150,
+    alignSelf: 'center',
+  },
+  titleText: {
+    fontSize: 17,
+    textAlign: 'center',
+    marginTop: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#a57bfb',
+    padding: 10,
+    margin: 10,
+    
   },
 });
